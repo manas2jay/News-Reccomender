924e4df86d7e2d30a08197208583154b4e4be7f5
Manas jain <manas2jay@gmail.com>
2021-03-03 12:15:23 +0530

    add local host in allowed host in settings.py

--- NEWSS/settings.py
+++ NEWSS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'fe37)*divs%!d6isink-aay21z35k#m$$gp-3i947a)%c*hj@h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['collectnews.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1:8000','collectnews.herokuapp.com']
 
 # Application definition
 
