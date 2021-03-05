d528aa3a49b08dd83b9fbdef8636b75c0997deb0
Manas jain <manas2jay@gmail.com>
2021-03-05 22:54:26 +0530

    updating setting and add runtime

--- NEWSS/settings.py
+++ NEWSS/settings.py
@@ -42,19 +42,23 @@ INSTALLED_APPS = [
     'rest_framework',
     'drf_multiple_model',
     'CategoryNews',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'NEWSS.urls'
 
 TEMPLATES = [
